7477b8a225633a4db0e6422df238ac36d3b06a99
Chris <chris.kuiper@gmail.com>
2017-08-17 07:20:05 -0700

    ran scaffolding with postgresql

---
+++ MvcMoviePg/Models/MvcMovieContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcMoviePg.Models
+{
+
+	public class MvcMovieContext : DbContext
+	{
+		public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+			: base(options)
+		{
+		}
+
+		public DbSet<MvcMoviePg.Models.Movie> Movie { get; set; }
+	}
+}
